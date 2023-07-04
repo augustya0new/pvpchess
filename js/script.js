f53c61b4dc338f5bc309129702101f2b9d946c34
Kumar Augustya <81870370+augustya0new@users.noreply.github.com>
2023-07-04 20:19:08 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,76 +1,47 @@
 // script.js
 
-// Variables to keep track of selected piece and destination square
-let selectedPiece = null;
-let destinationSquare = null;
+// Add event listener to all squares
+const squares = document.querySelectorAll('.square');
+squares.forEach(square => square.addEventListener('click', handleSquareClick));
 
-// Event listener for the chessboard squares
-const chessboardSquares = document.querySelectorAll("#chessboard div");
-chessboardSquares.forEach(square => {
-    square.addEventListener("click", () => {
-        // If a piece is already selected
-        if (selectedPiece) {
-            // Get the ID of the clicked square
-            const squareId = square.getAttribute("id");
-            
-            // If the clicked square is the same as the selected piece, deselect it
-            if (selectedPiece === square) {
-                square.classList.remove("selected");
-                selectedPiece = null;
-            }
-            // If the clicked square is a valid move for the selected piece, move it
-            else if (isValidMove(selectedPiece, squareId)) {
-                movePiece(selectedPiece, square);
-                selectedPiece = null;
-            }
-            // If the clicked square is not a valid move, select a new piece
-            else {
-                selectedPiece.classList.remove("selected");
-                square.classList.add("selected");
-                selectedPiece = square;
-            }
-        }
-        // If no piece is selected, select the clicked piece
-        else {
-            square.classList.add("selected");
-            selectedPiece = square;
-        }
-    });
-});
+// Track selected square
+let selectedSquare = null;
 
-// Function to check if a move is valid
-function isValidMove(piece, destination) {
-    // Implement your own logic here to validate the move
-    // For example, you can check if the move follows the rules of chess
-    // and if the destination square is empty or contains an opponent's piece
-    // Return true if the move is valid, false otherwise
-    return true;
+// Handle square click event
+function handleSquareClick() {
+    const currentSquare = this;
+  
+    // If a square is already selected, move the piece
+    if (selectedSquare) {
+        movePiece(selectedSquare, currentSquare);
+        clearSelection();
+    } else {
+        // If no square is selected, select the current square
+        selectSquare(currentSquare);
+    }
 }
 
-// Function to move a piece to the destination square
-function movePiece(piece, destination) {
-    // Implement your own logic here to move the piece
-    // For example, you can update the position of the piece on the chessboard
-    // and update the visual representation of the chessboard
-    // based on the selected piece and destination square
-    piece.classList.remove("selected");
-    destination.classList.add("selected");
-    piece.innerHTML = "";
-    destination.innerHTML = piece.innerHTML;
+// Select a square
+function selectSquare(square) {
+    if (selectedSquare) {
+        selectedSquare.classList.remove('selected');
+    }
+    square.classList.add('selected');
+    selectedSquare = square;
 }
 
-// Additional features
+// Move the piece to the target square
+function movePiece(sourceSquare, targetSquare) {
+    // Check if there is a piece in the source square
+    if (sourceSquare.innerHTML) {
+        // Move the piece by copying its HTML to the target square
+        targetSquare.innerHTML = sourceSquare.innerHTML;
+        sourceSquare.innerHTML = '';
+    }
+}
 
-// Reset button to clear the chessboard
-const resetButton = document.getElementById("reset-button");
-resetButton.addEventListener("click", () => {
-    // Clear the selected piece and destination square
-    selectedPiece = null;
-    destinationSquare = null;
-
-    // Reset the visual representation of the chessboard
-    chessboardSquares.forEach(square => {
-        square.innerHTML = "";
-        square.classList.remove("selected");
-    });
-});
+// Clear the selection
+function clearSelection() {
+    selectedSquare.classList.remove('selected');
+    selectedSquare = null;
+}
